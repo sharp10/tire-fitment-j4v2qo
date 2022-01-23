44c67e0470045cbbc3b869b785fa80968cb7191d
Pratik Shah <pratikshah@Pratiks-MacBook-Air.local>
2022-01-23 13:36:27 +0530

    Resolved some dropdown issue for null values
    Resolved some dropdown issue for null values
    
    Resolved some dropdown issue for null values
    
    Resolved some dropdown issue for null values
    
    Dropdown issue solved
    
    1 file modified

--- src/fitment/fitment-container/fitment-container.component.ts
+++ src/fitment/fitment-container/fitment-container.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { Store } from "@ngrx/store";
-import { Observable } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 import { VehicleAction } from '../store/actions';
 import * as fromVehicle from '../store/reducers';
 
 @Component({
-  selector: "app-fitment-container",
-  templateUrl: "./fitment-container.component.html",
-  styleUrls: ["./fitment-container.component.css"]
+  selector: 'app-fitment-container',
+  templateUrl: './fitment-container.component.html',
+  styleUrls: ['./fitment-container.component.css'],
 })
 export class FitmentContainerComponent implements OnInit {
   years$: Observable<any>;
@@ -21,18 +21,21 @@ export class FitmentContainerComponent implements OnInit {
   selectedModel: string;
   selectedTrim: string;
   // import the store into the constructor
-  constructor(private store: Store) { }
+  constructor(private store: Store) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  resetAll() {    
+  resetAll() {
     this.store.dispatch(VehicleAction.resetAll());
     this.years$ = this.store.select(fromVehicle.getYears);
     this.makes$ = this.store.select(fromVehicle.getMakes);
     this.models$ = this.store.select(fromVehicle.getModels);
     this.trims$ = this.store.select(fromVehicle.getTrims);
-    this.selectedYear = this.selectedMake = this.selectedModel = this.selectedTrim = ''
+    this.selectedYear =
+      this.selectedMake =
+      this.selectedModel =
+      this.selectedTrim =
+        '';
   }
 
   getYears() {
@@ -45,41 +48,52 @@ export class FitmentContainerComponent implements OnInit {
   getMakes(year: string) {
     // Make with year (2021)
     // https://6080be3273292b0017cdbf2a.mockapi.io/makes
+    this.selectedYear = '';
     this.store.dispatch(VehicleAction.clearMake());
     this.store.dispatch(VehicleAction.clearModel());
     this.store.dispatch(VehicleAction.clearTrim());
-    this.store.dispatch(VehicleAction.loadMakes({ year }));
-    this.makes$ = this.store.select(fromVehicle.getMakes);
-    this.selectedYear = year;
-    this.selectedMake = this.selectedModel = this.selectedTrim = ''
+    if (year != '') {
+      this.store.dispatch(VehicleAction.loadMakes({ year }));
+      this.makes$ = this.store.select(fromVehicle.getMakes);
+      this.selectedYear = year;
+    }
+    this.selectedMake = this.selectedModel = this.selectedTrim = '';
   }
 
   getModels(make: string) {
     // Model with year and make (Acura)
     // https://6080be3273292b0017cdbf2a.mockapi.io/models
+    this.selectedMake = '';
     this.store.dispatch(VehicleAction.clearModel());
     this.store.dispatch(VehicleAction.clearTrim());
-    var payload = { year: this.selectedYear, make: make };
-    this.store.dispatch(VehicleAction.loadModels(payload));
-    this.selectedMake = make;
-    this.models$ = this.store.select(fromVehicle.getModels);
-    this.selectedModel = this.selectedTrim = ''
+    if (make != '') {
+      var payload = { year: this.selectedYear, make: make };
+      this.store.dispatch(VehicleAction.loadModels(payload));
+      this.selectedMake = make;
+      this.models$ = this.store.select(fromVehicle.getModels);
+    }
+    this.selectedModel = this.selectedTrim = '';
   }
 
   getTrims(model: string) {
     // Trim with year, make, model (RDX)
     // https://6080be3273292b0017cdbf2a.mockapi.io/trim
+    this.selectedModel = '';
     this.store.dispatch(VehicleAction.clearTrim());
-    var payload = { year: this.selectedYear, make: this.selectedMake, model: model };
-    this.store.dispatch(VehicleAction.loadTrims(payload));
-    this.selectedModel = model;
-    this.trims$ = this.store.select(fromVehicle.getTrims);
-    this.selectedTrim = ''
+    if (model != '') {
+      var payload = {
+        year: this.selectedYear,
+        make: this.selectedMake,
+        model: model,
+      };
+      this.store.dispatch(VehicleAction.loadTrims(payload));
+      this.selectedModel = model;
+      this.trims$ = this.store.select(fromVehicle.getTrims);
+    }
+    this.selectedTrim = '';
   }
 
-  getData(trim: string) {
+  getTyres(trim: string) {
     this.selectedTrim = trim;
-    // Trim with year, make, model (RDX)
-    // https://6080be3273292b0017cdbf2a.mockapi.io/trim
   }
 }
